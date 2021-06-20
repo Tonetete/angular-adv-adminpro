b8e9cc56e79870b066ac8abe2e0532586ff21167
Antonio Rivas <toneterivas@gmail.com>
2021-06-20 23:07:58 +0200

    end section 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graph1Component } from './pages/graph1/graph1.component';
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'progress',
+        component: ProgressComponent,
+      },
+      {
+        path: 'graph1',
+        component: Graph1Component,
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '**',
+    component: NotPageFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/not-page-found/not-page-found.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-page-found',
+  templateUrl: './not-page-found.component.html',
+  styleUrls: ['./not-page-found.component.scss'],
+})
+export class NotPageFoundComponent implements OnInit {
+  year = new Date().getFullYear();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
